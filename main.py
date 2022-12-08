be764785a1f6f433c23a657a39a0f7c0e19d4449
Artificialheaven <69500511+Artificialheaven@users.noreply.github.com>
2022-12-08 16:29:42 +0800

    增加函数功能

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-while True:  #启动循环
-  cmd = input(">>>")   #请求输入
-  exec(cmd)  #运行指令
+import traceback
+
+
+temp = ''							# 代码缓冲区
+func_list = []						# 函数列表
+global_var_table = {}				# 全局变量列表, 每次都要继承
+while True:  						# 启动循环
+	try:
+		cmd = input(">>>")   			# 请求输入
+		if cmd.startswith('def'):		# 创建函数
+			while True:
+				if cmd == '':				# 函数结束
+					d, _d = {}, {}
+					exec(temp, global_var_table)
+					break
+				else:
+					temp = temp + '\n' + cmd
+					cmd = input('...')
+		else:
+			exec(cmd, global_var_table)  	# 运行指令
+	except Exception as e:
+		traceback.print_exc()			# 获取全部报错
---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+title 'Pytjen 1.2.7'
+echo 'Pytjen 1.2.7 for Windows10_x86 (2022/11/30)'
+python main.py
